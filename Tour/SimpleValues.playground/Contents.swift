62ab5872af6aa62804b8ee231ad07d18ec325109
Thao Nguyen <thaonh@sigma-solutions.eu>
2018-10-19 17:50:23 +0700

    Swift tour day 1

---
+++ Tour/SimpleValues.playground/Contents.swift
@@ -0,0 +1,19 @@
+print("Hello, world!")
+
+var myVariable = 42
+myVariable = 50
+let myConstant = 42
+
+let implicitInteger = 70
+let implicitDouble = 70.0
+let explicitDouble: Double = 70
+let floatConstant: Float = 4
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."

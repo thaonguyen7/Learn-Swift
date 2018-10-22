968d2d8367104733b28715205f10efcd3a4f40b5
Thao Nguyen <thaonh@sigma-solutions.eu>
2018-10-22 17:30:53 +0700

    Done simple values section

--- Tour/SimpleValues.playground/Contents.swift
+++ Tour/SimpleValues.playground/Contents.swift
@@ -17,3 +17,35 @@ let apples = 3
 let oranges = 5
 let appleSummary = "I have \(apples) apples."
 let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+let rectWidth: Float = 5
+let rectHeight: Float = 4
+let rectArea = "Rect area is \(rectWidth * rectHeight)"
+let name = "LK"
+let greeting = "Hello, \(name)"
+
+let quotation = """
+Even though there's whitespace to the left,
+the actual lines aren't indented.
+Except for this line.
+Double quotes (") can appear without being escaped.
+I still have \(apples + oranges) pieces of fruit.
+"""
+
+var shoppingList = ["catfish", "water", "tulips",]
+shoppingList[1] = "bottle of water"
+
+var occupations = [
+    "Malcolm": "Captain",
+    "Kaylee": "Mechanic",
+]
+occupations["Jayne"] = "Public Relations"
+
+shoppingList.append("blue paint")
+//shoppingList[5] = "test" *compile error because just have 4 elements in the array
+
+let emptyArray = [String]()
+let emptyDictionary = [String: Float]()
+
+shoppingList = []
+occupations = [:]
